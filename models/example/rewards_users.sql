92e67710a25bc0f25b5ccb1962286269470cff11
Rashmi Mishra <rashmimishra@MacBook-Pro-8.local>
2022-07-11 13:07:37 +0530

    remove timestamp conversion

--- models/example/rewards_users.sql
+++ models/example/rewards_users.sql
@@ -19,7 +19,7 @@
 
 
 select
-  id, user_id, first_name, last_name, email, contact, gender, to_timestamp(dob)::timestamp as dob, location, latitude, longitude, work_status, preferred_languages, is_mother, family_type, interests, durables, to_timestamp(created_time)::timestamp as created_time, to_timestamp(updated_time)::timestamp as updated_time, is_deleted, profession_type, income_slab, device_details, is_email_verified, referral_code, referred_by, state_name, is_email_invalid, is_rewards_disabled, rewards_app_login, is_expecting, helpdesk_membership_id, city, user_tags, rfm_cluster_score
+  id, user_id, first_name, last_name, email, contact, gender, to_timestamp(dob) as dob, location, latitude, longitude, work_status, preferred_languages, is_mother, family_type, interests, durables, to_timestamp(created_time) as created_time, to_timestamp(updated_time) as updated_time, is_deleted, profession_type, income_slab, device_details, is_email_verified, referral_code, referred_by, state_name, is_email_invalid, is_rewards_disabled, rewards_app_login, is_expecting, helpdesk_membership_id, city, user_tags, rfm_cluster_score
 from users
 
 
